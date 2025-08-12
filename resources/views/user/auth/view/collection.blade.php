01fbf38da4f5b07a28ff5ef32092472a051614b9
Toee Thiha Kyaw <toeethihakyaw@gmail.com>
2025-08-12 20:45:32 +0630

    Improve UI of brand collection page.

--- resources/views/user/auth/view/collection.blade.php
+++ resources/views/user/auth/view/collection.blade.php
@@ -15,28 +15,36 @@
   }
 </style>
 <x-auth.user.layout title="Brands Collections">
-  @foreach ($brands as $brand)
-    <div class="p-4">
-      <div class="fs-5 bg-secondary-subtle px-2 ps-4 py-2 rounded d-flex justify-content-between align-items-center">
-        <div>{{ $brand->name }} Models</div>
-        <a href="{{ url('brands/devices?brand=' . $brand->id) }}">
-          <div class="btn btn-success fs-5">See All</div>
-        </a>
-      </div>
-      <div class="row p-1">
-        @if (count($brand->devices))
-          @foreach ($brand->devices as $device)
-            <x-auth.user.card.card :device="$device" />
-          @endforeach
-        @else
-          <div class="mt-5 d-flex justify-content-center">
-            <div class="text-center p-4">
-              <h5 class="text-muted mb-0">No devices found.</h5>
+  @if (count($brands))
+    @foreach ($brands as $brand)
+      <div class="p-4">
+        <div class="fs-5 bg-secondary-subtle px-2 ps-4 py-2 rounded d-flex justify-content-between align-items-center">
+          <div>{{ $brand->name }} Models</div>
+          <a href="{{ url('brands/devices?brand=' . $brand->id) }}">
+            <div class="btn btn-success fs-5">See All</div>
+          </a>
+        </div>
+        <div class="row p-1">
+          @if (count($brand->devices))
+            @foreach ($brand->devices as $device)
+              <x-auth.user.card.card :device="$device" />
+            @endforeach
+          @else
+            <div class="mt-5 d-flex justify-content-center">
+              <div class="text-center p-4">
+                <h5 class="text-muted mb-0">No devices found.</h5>
+              </div>
             </div>
-          </div>
-        @endif
+          @endif
 
+        </div>
+      </div>
+    @endforeach
+  @else
+    <div class="mt-5 d-flex justify-content-center">
+      <div class="text-center p-4">
+        <h5 class="text-muted mb-0">No devices found.</h5>
       </div>
     </div>
-  @endforeach
+  @endif
 </x-auth.user.layout>
